c2051b7cc843a20cf246cca13ba6d7447d056015
THODDUSAMY G <103572861+thoddusamy@users.noreply.github.com>
2022-06-11 11:56:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,13 +82,33 @@ formSubmit.addEventListener('click', (page) => {
 
   let pincodeData1 = (document.getElementById('pincode-txt').value = '')
 
-  let genderData1 = (document.getElementsByName('gender').value = '')
-
-  let foodData1 = (document.getElementsByName('foods').value = '')
-
   let stateData1 = (document.getElementById('state-txt').value = '')
 
   let countryData1 = (document.getElementById('country-txt').value = '')
+
+  let uncheck1 = document.querySelector('#chicken')
+  uncheck1.checked = false
+
+  let uncheck2 = document.querySelector('#mutton')
+  uncheck2.checked = false
+
+  let uncheck3 = document.querySelector('#fish')
+  uncheck3.checked = false
+
+  let uncheck4 = document.querySelector('#pizza')
+  uncheck4.checked = false
+
+  let uncheck5 = document.querySelector('#burger')
+  uncheck5.checked = false
+
+  let radio1 = document.querySelector('#gender-male')
+  radio1.checked = false
+
+  let radio2 = document.querySelector('#gender-female')
+  radio2.checked = false
+
+  let radio3 = document.querySelector('#gender-other')
+  radio3.checked = false
 
   page.preventDefault()
 })
